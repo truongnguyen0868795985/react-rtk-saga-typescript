f232e67c79ba69e0aa2e016b03433a325cdd6c6c
TruongNguyen <truongnguyen0868795985>
2022-01-24 14:26:18 +0700

    Student: show table student

--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,10 +1,28 @@
+import { Box, Button, Typography, makeStyles } from '@material-ui/core';
 import React, { useEffect } from 'react';
+import { selectStudentList, studentActions } from '../studentSlice';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 
-import { studentActions } from '../studentSlice';
-import { useAppDispatch } from 'app/hooks';
+import StudentTable from '../components/StudentTable';
+
+const useStyle = makeStyles((theme) => ({
+  root: {},
+
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: theme.spacing(4),
+  },
+}));
 
 export default function ListPage() {
   const dispatch = useAppDispatch();
+
+  const classes = useStyle();
+
+  const studentList = useAppSelector(selectStudentList);
 
   useEffect(() => {
     dispatch(
@@ -15,5 +33,16 @@ export default function ListPage() {
     );
   }, [dispatch]);
 
-  return <div>Student list page</div>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h4">Student</Typography>
+        <Button variant="contained" color="primary">
+          Add new student
+        </Button>
+      </Box>
+
+      <StudentTable studentList={studentList} onEdit={() => {}} onRemove={() => {}}></StudentTable>
+    </Box>
+  );
 }
